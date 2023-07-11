83bd5082382b74320012cc2e2d2ce0c95c667481
sairammadala <sairammadala7101@gmail.com>
2023-07-12 01:19:18 +0530

    Initial commit

---
+++ src/components/Weather.js
@@ -0,0 +1,86 @@
+import React, { useState } from "react";
+import DisplayWeather from "./DisplayWeather";
+import "./weather.css";
+
+function Weather() {
+  const [weather, setWeather] = useState([]);
+  const [form, setForm] = useState({
+    city: "",
+    country: "",
+  });
+  const [len, setlen] = useState(0)
+  let lat= 16.515099;
+  let lon = 80.632095;
+
+  const APIKEY = "d35e660615d6835768d6e06d50623788";
+  
+  async function weatherData(e) {
+    e.preventDefault();
+    if (form.city === "") {
+      alert("Add values");
+    } else {
+      const ldata = await fetch(
+        `http://api.openweathermap.org/geo/1.0/direct?q=${form.city},${form.country}&limit=${5}&appid=${APIKEY}`
+      )
+      .then((res) => res.json())
+      .then((ldata) => ldata)
+      setlen(ldata.length)
+      
+      lat = ldata[0].lat
+      lon = ldata[0].lon
+
+      const data = await fetch(
+        `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${APIKEY}`
+      )
+        .then((res) => res.json())
+        .then((data) => data);
+
+      setWeather({...weather, data: data });
+    }
+  }
+
+  const handleChange = (e) => {
+    let name = e.target.name;
+    let value = e.target.value;
+
+    if (name === "city") {
+      setForm({ ...form, city: value });
+    }
+    if (name === "country") {
+      setForm({ ...form, country: value });
+    }
+  };
+  return (
+    <div className="weather">
+      <span className="title">Weather App</span>
+      <br />
+      <form>
+        <input
+          type="text"
+          placeholder="city"
+          name="city"
+          onChange={(e) => handleChange(e)}
+        />
+        &nbsp; &nbsp; &nbsp;&nbsp;
+        <input
+          type="text"
+          placeholder="Country"
+          name="country"
+          onChange={(e) => handleChange(e)}
+        />
+        <button className="getweather" onClick={(e) => weatherData(e)}>
+          Submit
+        </button>
+      </form>
+
+      {/* {console.log(weather)} */}
+      {weather.data !== undefined ? (
+        <div>
+          <DisplayWeather data={weather.data} len={len} />
+        </div>
+      ) : null}
+    </div>
+  );
+}
+
+export default Weather;
